899890daabe7a9a3cf314ee29d16c0ee41331efc
RotemCarmon <tetch6@gmail.com>
2022-02-20 14:39:51 +0200

    Added fake expenses to fake data in group service

--- src/modules/group/services/group.service.js
+++ src/modules/group/services/group.service.js
@@ -34,7 +34,8 @@ function getEmptyGroup() {
   return {
     name: '',
     description: '',
-    members: []
+    members: [],
+    expenses: {}
   }
 }
 
@@ -48,7 +49,12 @@ function _getDefaultGroups() {
         { id: 103, name: 'Rotem', email: 'tetch6@gmail.com', isOwner: true },
         { id: 104, name: 'Ben', email: 'benY@gmail.com' },
         { id: 105, name: 'Nave', email: 'nane@gmail.com' },
-      ]
+      ],
+      expenses: {
+        'Rotem': [{ amount: 10, exclude: ['Ben'] }],
+        'Ben': [{ amount: 21, exclude: [] }],
+        'Nave': [],
+      }
     },
     {
       _id: makeId(8),
@@ -57,7 +63,11 @@ function _getDefaultGroups() {
       members: [
         { id: 101, name: 'Popo', email: 'popo@gmail.com', isOwner: true },
         { id: 102, name: 'Momo', email: 'momo@gmail.com' }
-      ]
+      ],
+      expenses: {
+        'Popo': [],
+        'Momo': [],
+      }
     }
   ]
 }

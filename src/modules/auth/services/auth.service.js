41090d7d7bfb9c93f13812449c734bba6dced7cf
RotemCarmon <tetch6@gmail.com>
2022-02-21 23:51:43 +0200

    Added auth functions

--- src/modules/auth/services/auth.service.js
+++ src/modules/auth/services/auth.service.js
@@ -1,3 +1,52 @@
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut } from "firebase/auth";
+import { userService } from './user.service.js'
+const auth = getAuth()
+
+const USER_SESSION_KEY = 'loggedInUser';
+
+async function login(userCreds) {
+  const { email, password } = userCreds
+  const { user } = await signInWithEmailAndPassword(auth, email, password)
+  const loggedInUser = await userService.getUserById(user.uid)
+  _saveLocalUser(loggedInUser)
+  return loggedInUser
+
+}
+
+async function signup(creds) {
+  const { user } = await createUserWithEmailAndPassword(
+    auth,
+    creds.email,
+    creds.password
+  );
+  creds.id = user.uid
+  delete creds.password
+  const loggedInUser = await userService.addUser(creds)
+  _saveLocalUser(loggedInUser)
+  return loggedInUser
+}
+
+async function signout() {
+  await signOut(auth)
+  clearLoggedUser
+}
+
 export const authService = {
- 
+  login,
+  signup,
+  signout,
+  getLoggedinUser
+}
+
+function getLoggedinUser() {
+  return JSON.parse(sessionStorage.getItem(USER_SESSION_KEY));
+}
+
+function _saveLocalUser(user) {
+  sessionStorage.setItem(USER_SESSION_KEY, JSON.stringify(user));
+  return user;
+}
+
+function clearLoggedUser() {
+  sessionStorage.removeItem(USER_SESSION_KEY);
 }
--- src/modules/auth/store/auth.store.js
+++ src/modules/auth/store/auth.store.js
@@ -1,10 +1,54 @@
-import {authService} from '../services/auth.service.js'
+import { authService } from '../services/auth.service.js'
+import { loggerService } from '@/modules/common/services/logger.service.js'
+import { popupService } from '@/modules/common/services/popup.service.js'
+import { router } from '@/router'
 
 export default {
-namespaced: true,
-state: {},
-getters: {},
-mutations: {},
-actions: {},
-modules: {}
+  namespaced: true,
+  state: {
+    loggedInUser: null
+  },
+  getters: {
+    loggedInUser({ loggedInUser }) { return loggedInUser }
+  },
+  mutations: {
+    setLoggedInUser(state, { loggedInUser }) {
+      state.loggedInUser = loggedInUser
+    }
+
+  },
+  actions: {
+    async login({commit }, { userCreds }) {
+      try {
+        const user = await authService.login(userCreds)
+        console.log('user:', user)
+        commit({ type: 'setLoggedInUser', loggedInUser: user })
+        router.push('/')
+      } catch (err) {
+        loggerService.error(err)
+      }
+    },
+    async signup({ commit }, { userCreds }) {
+      try {
+        const user = await authService.signup(userCreds)
+        console.log('user:', user)
+        commit({ type: 'setLoggedInUser', loggedInUser: user })
+        popupService.success('User registered successfuly')
+        router.push('/')
+
+      } catch (err) {
+        loggerService.error(err)
+      }
+    },
+    async signout({ commit }, ) {
+      try {
+        await authService.signout()
+        commit({ type: 'setLoggedInUser', loggedInUser: null })
+        router.push('/auth')
+
+      } catch (err) {
+        loggerService.error(err)
+      }
+    }
+  },
 };

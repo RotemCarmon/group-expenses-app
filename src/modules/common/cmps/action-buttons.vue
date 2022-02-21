899f995798872ce49bbf14807c397a3f87fd4531
RotemCarmon <tetch6@gmail.com>
2022-02-21 19:46:09 +0200

    Stopped propagation from action buttons cmp

--- src/modules/common/cmps/action-buttons.vue
+++ src/modules/common/cmps/action-buttons.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="action-buttons-container">
-    <img @click="edit" :src="require('@/assets/icons/edit.svg')" />
-    <img @click="remove" :src="require('@/assets/icons/delete.svg')" />
+    <img @click.stop="edit" :src="require('@/assets/icons/edit.svg')" />
+    <img @click.stop="remove" :src="require('@/assets/icons/delete.svg')" />
   </section>
 </template>
 

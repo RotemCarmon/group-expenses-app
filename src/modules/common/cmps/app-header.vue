d7dc9c547e39de06d68facc57534f2a2a874029c
RotemCarmon <tetch6@gmail.com>
2022-02-21 19:45:21 +0200

    Added back button in header

--- src/modules/common/cmps/app-header.vue
+++ src/modules/common/cmps/app-header.vue
@@ -1,17 +1,32 @@
 <template>
   <section class="app-header-container">
-    <div class="hamburger-menu">
-      <img :src="require('@/assets/icons/menu.svg')" alt="">
+    <div class="back" @click="back" v-if="hasBack">
+      <img :src="require('@/assets/icons/arrow-left.svg')" />
     </div>
   </section>
 </template>
 
 <script>
 export default {
+  name: 'app-header',
+  methods: {
+    back() {
+      let homeRoute = '/';
+      if(this.$route.name === 'expense-edit') {
+        homeRoute = `/group/${this.$route.params.groupId}`;
 
-}
+      } else if (this.$route.path.includes('group')) {
+        homeRoute = '/group';
+      }
+
+      this.$router.replace(homeRoute);
+    },
+  },
+  computed: {
+    hasBack() {
+      const noBackCmps = ['group-list'];
+      return !noBackCmps.includes(this.$route.name);
+    },
+  }
+};
 </script>
-
-<style>
-
-</style>

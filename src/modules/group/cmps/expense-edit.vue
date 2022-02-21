66562138aa5b1b27ae7634c2902a3d5afd96e902
RotemCarmon <tetch6@gmail.com>
2022-02-21 10:48:40 +0200

    Changed expense-edit cmp to use multi-select cmp

--- src/modules/group/cmps/expense-edit.vue
+++ src/modules/group/cmps/expense-edit.vue
@@ -1,27 +1,39 @@
 <template>
   <section class="expense-edit-container container">
     <main>
+      <div class="group-name">Group <span>Bansko 2022</span></div>
       <div class="page-header">
         <div class="title">{{ edit ? 'Edit' : 'Add' }} Expense</div>
       </div>
-      <input type="text" class="form-input" placeholder="Enter Amount" />
-      <label for="">
+      <input
+        type="text"
+        class="form-input form-row"
+        placeholder="Enter Amount"
+      />
+      <label class="currency-select form-row">
         Currency
-        <select class="form-select">
-          <option value="USD">USD</option>
-          <option value="EUR">EUR</option>
-          <option value="NIS">NIS</option>
-        </select>
+        <multi-select
+          :items="currencies"
+          :isMulti="false"
+          
+        />
       </label>
-      <input type="text" class="form-input" placeholder="Enter Description" />
-      <h3>Who is the spender?</h3>
-      <select class="form-select">
-        <option value="Rotem">Rotem</option>
-        <option value="Ben">Ben</option>
-        <option value="Nave">Nave</option>
-      </select>
-      <h3>Exclude</h3>
-      <multi-select @change="updateExcludes" :items="members" placeholder="Who to exclude?" />
+      <input
+        type="text"
+        class="form-input form-row"
+        placeholder="Enter Description"
+      />
+      <h3 class="sub-title">Who is the spender?</h3>
+      <multi-select
+        :items="members"
+        :isMulti="false"
+        class="form-row"
+      />
+      <h3 class="sub-title">Exclude</h3>
+      <multi-select
+        :items="members"
+        placeholder="Who to exclude?"
+      />
     </main>
     <button class="btn dark buttom-btn">Submit</button>
   </section>
@@ -35,13 +47,13 @@ export default {
     return {
       edit: false,
       members: ['Rotem', 'Ben', 'Nave'],
+      currencies: ['USD', 'EUR', 'NIS'],
     };
   },
   methods: {
     updateExcludes(excludes) {
-      console.log('excludes:', excludes) 
-      
-    }
+      console.log('excludes:', excludes);
+    },
   },
   components: {
     multiSelect,

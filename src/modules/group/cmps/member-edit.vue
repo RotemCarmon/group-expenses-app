88577e7319c91ffce90c762478b0766d80dad4ea
RotemCarmon <tetch6@gmail.com>
2022-02-22 18:51:08 +0200

    Added ability to save and update member in member-edit cmp

--- src/modules/group/cmps/member-edit.vue
+++ src/modules/group/cmps/member-edit.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="member-edit-container container">
+  <section class="member-edit-container container" v-if="memberToEdit">
     <main>
       <button @click="$emit('close')" class="close-btn">
         <img :src="require('@/assets/icons/close.svg')" />
@@ -7,16 +7,41 @@
       <div class="page-header">
         <div class="title">Add Member</div>
       </div>
-      <input type="text" class="form-input" placeholder="Enter Name" />
-      <input type="text" class="form-input" placeholder="Enter Email" />
+      <input
+        type="text"
+        class="form-input"
+        placeholder="Enter Name"
+        v-model="memberToEdit.name"
+      />
+      <input
+        type="text"
+        class="form-input"
+        placeholder="Enter Email"
+        v-model="memberToEdit.email"
+      />
     </main>
-    <button class="btn dark buttom-btn create-btn">Save</button>
+    <button @click="save" class="btn dark buttom-btn create-btn">Save</button>
   </section>
 </template>
 
 <script>
-export default {};
+export default {
+  name: 'member-edit',
+  props: {
+    member: { type: Object },
+  },
+  data() {
+    return {
+      memberToEdit: null,
+    };
+  },
+  methods: {
+    save() {
+      this.$emit('save', this.memberToEdit);
+    },
+  },
+  created() {
+    this.memberToEdit = this.member;
+  },
+};
 </script>
-
-<style>
-</style>

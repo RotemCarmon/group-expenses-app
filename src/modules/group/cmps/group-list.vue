9253495d6cf7e6fc4f2717837e1ccc2d9c3b6843
RotemCarmon <tetch6@gmail.com>
2022-02-22 18:50:31 +0200

    Changed _id to id in group-list cmp

--- src/modules/group/cmps/group-list.vue
+++ src/modules/group/cmps/group-list.vue
@@ -4,7 +4,7 @@
       <div class="title">My Groups</div>
     </div>
     <div class="group-list">
-      <group-preview v-for="group in groups" :key="group._id" :group="group" />
+      <group-preview v-for="group in groups" :key="group.id" :group="group" />
     </div>
     <button @click="goToAddGroup" class="add-group btn-dashed">
       + Add Group
@@ -13,7 +13,6 @@
 </template>
 
 <script>
-import { popupService } from '@/modules/common/services/popup.service.js';
 import groupPreview from './group-preview';
 export default {
   name: 'group-list',

e997eb19632c324714411654d23c42d245a64271
RotemCarmon <tetch6@gmail.com>
2022-02-22 15:12:41 +0200

    using firestore service for in user service

--- src/modules/auth/services/user.service.js
+++ src/modules/auth/services/user.service.js
@@ -1,22 +1,14 @@
-import { getFirestore, collection, setDoc, doc, getDoc } from "firebase/firestore"
-const db = getFirestore()
+import { firebaseService } from '@/modules/common/services/firestore.service.js';
 
 const COLLECTION_NAME = 'user'
-const USER_SESSION_KEY = 'loggedInUser';
 
 async function addUser(user) {
-  await setDoc(doc(db, COLLECTION_NAME, user.id), user);
+  await firebaseService.post(COLLECTION_NAME, user)
   return user
 }
 
 async function getUserById(userId) {
-  const docRef = doc(db, COLLECTION_NAME, userId);
-  const docSnap = await getDoc(docRef);
-  if (docSnap.exists()) {
-    return docSnap.data()
-  } else {
-    throw new Error('Can\'t find user with id: ' + userId)
-  }
+  return await firebaseService.get(COLLECTION_NAME, userId)
 }
 
 export const userService = {

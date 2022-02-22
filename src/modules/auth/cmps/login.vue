06b7f1f02c5e2601dce239eaaf361da51851aaf4
RotemCarmon <tetch6@gmail.com>
2022-02-22 13:55:05 +0200

    Added keyup.enter to login cmp

--- src/modules/auth/cmps/login.vue
+++ src/modules/auth/cmps/login.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="login-signup-container container">
+  <section class="login-signup-container container" @keyup.enter="doLogin">
     <div class="page-header">
       <div class="title">Sign In</div>
     </div>

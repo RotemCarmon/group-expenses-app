cb12dd98f901757adaf81e7768a9b061d53f6671
RotemCarmon <tetch6@gmail.com>
2022-02-25 21:42:47 +0200

    reordered expense-edit cmp

--- src/modules/group/cmps/expense-edit.vue
+++ src/modules/group/cmps/expense-edit.vue
@@ -1,7 +1,9 @@
 <template>
-  <section class="expense-edit-container container">
+  <section class="expense-edit-container container" v-if="group">
     <main>
-      <div class="group-name">Group <span>Bansko 2022</span></div>
+      <div class="group-name">
+        Group <span>{{ group.name }}</span>
+      </div>
       <div class="page-header">
         <div class="title">{{ edit ? 'Edit' : 'Add' }} Expense</div>
       </div>
@@ -9,51 +11,88 @@
         type="text"
         class="form-input form-row"
         placeholder="Enter Amount"
+        v-model.number="expenseToEdit.amount"
       />
-      <label class="currency-select form-row">
-        Currency
-        <multi-select
-          :items="currencies"
-          :isMulti="false"
-          
-        />
-      </label>
       <input
         type="text"
         class="form-input form-row"
         placeholder="Enter Description"
+        v-model="expenseToEdit.description"
       />
+      <div class="currency-select form-row">
+        Currency
+        <multi-select
+          :items="currencyCodes"
+          :isMulti="false"
+          :topSelections="['USD', 'EUR']"
+          v-model="expenseToEdit.currency"
+        />
+      </div>
+
       <h3 class="sub-title">Who is the spender?</h3>
       <multi-select
         :items="members"
         :isMulti="false"
         class="form-row"
+        v-model="spender"
       />
       <h3 class="sub-title">Exclude</h3>
       <multi-select
         :items="members"
         placeholder="Who to exclude?"
+        v-model="expenseToEdit.exclude"
       />
     </main>
-    <button class="btn dark buttom-btn">Submit</button>
+    <button @click="saveExpense" class="btn dark buttom-btn">Submit</button>
   </section>
 </template>
 
 <script>
+import { expenseService } from '../services/expense.service';
 import { multiSelect } from '@/modules/common/cmps/';
 export default {
   name: 'expense-edit',
   data() {
     return {
       edit: false,
-      members: ['Rotem', 'Ben', 'Nave'],
-      currencies: ['USD', 'EUR', 'NIS'],
+      group: null,
+      expenseToEdit: null,
+      spender: '', // loggedInUser
     };
   },
-  methods: {
-    updateExcludes(excludes) {
-      console.log('excludes:', excludes);
+  computed: {
+    members() {
+      return this.group?.members?.map((member) => member.name);
     },
+    currencyCodes() {
+      return this.$store.getters['commonStore/currencyCodes'];
+    },
+  },
+  methods: {
+    async getGroup() {
+      const { groupId } = this.$route.params;
+      if (groupId) {
+        const group = await this.$store.dispatch({
+          type: 'groupStore/getGroupById',
+          groupId,
+        });
+        this.group = group;
+      }
+    },
+    async getExpense() {
+      // check if expenseId exist
+      this.expenseToEdit = expenseService.getEmptyExpense();
+    },
+    saveExpense() {     
+      this.group.expenses[this.spender.toLowerCase()].push(this.expenseToEdit);
+      this.$store.dispatch({ type: 'groupStore/saveGroup', group: this.group });
+      this.$router.go(-1)
+    },
+  },
+  created() {
+    this.getGroup();
+    this.getExpense();
+    this.spender = this.$store.getters['authStore/loggedInUser']?.username;
   },
   components: {
     multiSelect,

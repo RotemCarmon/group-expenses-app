1aef2276af94ff740aa58670557dfc35b2e44a05
RotemCarmon <tetch6@gmail.com>
2023-03-16 12:50:00 +0000

    removed unused code

--- src/modules/group/services/group.service.js
+++ src/modules/group/services/group.service.js
@@ -52,36 +52,3 @@ function getEmptyMember() {
     id: makeId(8)
   }
 }
-
-function _getDefaultGroups() {
-  return [
-    {
-      id: makeId(8),
-      name: 'Bansko 2022',
-      description: 'Bansko trip calculating money to split',
-      members: [
-        { id: 103, name: 'Rotem', email: 'tetch6@gmail.com', isOwner: true },
-        { id: 104, name: 'Ben', email: 'benY@gmail.com' },
-        { id: 105, name: 'Nave', email: 'nane@gmail.com' },
-      ],
-      expenses: {
-        'Rotem': [{ amount: 10, exclude: ['Ben'] }],
-        'Ben': [{ amount: 21, exclude: [] }],
-        'Nave': [],
-      }
-    },
-    {
-      id: makeId(8),
-      name: 'BBQ at Yogev\'s',
-      description: 'Spliting money for meat and drinks',
-      members: [
-        { id: 101, name: 'Popo', email: 'popo@gmail.com', isOwner: true },
-        { id: 102, name: 'Momo', email: 'momo@gmail.com' }
-      ],
-      expenses: {
-        'Popo': [],
-        'Momo': [],
-      }
-    }
-  ]
-}

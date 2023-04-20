64ecc04d7def12b602d6bd83fe646f528f39c9d4
RotemCarmon <tetch6@gmail.com>
2023-04-20 16:42:47 +0100

    Expended the expense-preview to allow selecting expenses

--- src/modules/expense/cmps/expense-preview.vue
+++ src/modules/expense/cmps/expense-preview.vue
@@ -1,15 +1,19 @@
 <template>
-  <div class="expense-preview-container">
+  <div class="expense-preview-container" :class="{ selectable: isSelectable }">
+    <div class="select-box-wrapper" v-if="isSelectable">
+      <input type="checkbox" :checked="isSelected" @change="$emit('select', expense.id)" />
+    </div>
     <div class="name">{{ expense.name }}</div>
     <div class="date">
       {{ formatDate(expense.createdAt, 'D') }}
     </div>
-    <div class="ellipsis-icon" @click.stop="$emit('openMenu', expense)">
-      <img :src="require('@/assets/icons/ellipsis.svg')" />
-    </div>
     <div class="description">{{ expense.description }}</div>
     <div class="amount">{{ getSymbolFromCurrency(expense.currency) }}{{ expense.amount.toFixed(2) }}</div>
     <div class="exclude" v-if="expense.exclude && expense.exclude.length">Exclude: {{ expense.exclude.join(', ') }}</div>
+
+    <div class="ellipsis-icon" v-if="!isSelectable" @click.stop="$emit('openMenu', expense)">
+      <img :src="require('@/assets/icons/ellipsis.svg')" />
+    </div>
   </div>
 </template>
 
@@ -19,7 +23,7 @@ import getSymbolFromCurrency from 'currency-symbol-map';
 
 const props = defineProps({
   expense: { type: Object, required: true },
+  isSelectable: { type: Boolean, default: false },
+  isSelected: { type: Boolean, default: false },
 });
 </script>
-
-<style></style>

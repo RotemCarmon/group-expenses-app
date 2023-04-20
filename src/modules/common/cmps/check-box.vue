f2c108752545b2c0e1af0961e601a1c7e236ebb4
RotemCarmon <tetch6@gmail.com>
2023-04-20 18:06:14 +0100

    Added check-box cmp

---
+++ src/modules/common/cmps/check-box.vue
@@ -0,0 +1,14 @@
+<template>
+  <label class="check-box-container">
+    <input class="checkbox" type="checkbox" :checked="isChecked" @change="$emit('update:isChecked', !isChecked)" />
+    <span class="checkmark"></span>
+  </label>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const props = defineProps({
+  isChecked: { type: Boolean, default: false },
+});
+</script>

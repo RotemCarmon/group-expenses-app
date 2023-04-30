0651c69c9714879682158565e450fdb084d9439a
RotemCarmon <tetch6@gmail.com>
2023-04-30 16:04:03 +0100

    Wrapped member name in a span

--- src/modules/group/cmps/member-preview.vue
+++ src/modules/group/cmps/member-preview.vue
@@ -1,8 +1,8 @@
 <template>
   <section class="member-preview-container preview-grid">
     <div class="name">
-      {{ member.name }}
-      <span class="group-owner" v-if="member.isOwner">Owner</span>
+      <span data-testId="member-name">{{ member.name }}</span>
+      <span class="group-owner" data-testId="group-owner" v-if="member.isOwner">Owner</span>
     </div>
     <div class="ellipsis-icon" @click.stop="$emit('toggleMenu', member)">
       <img src="@/assets/icons/ellipsis.svg" />
@@ -15,7 +15,7 @@ const props = defineProps({
   member: { type: Object },
 });
 
-const emit = defineEmits(['edit','toggleMenu']);
+const emit = defineEmits(['edit', 'toggleMenu']);
 
 function editMember() {
   emit('edit', props.member);

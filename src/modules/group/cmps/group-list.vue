702ad42ef4f8693788f6699193979e1f0885bcd7
RotemCarmon <tetch6@gmail.com>
2023-06-08 17:20:23 +0200

    Added search box to group list

--- src/modules/group/cmps/group-list.vue
+++ src/modules/group/cmps/group-list.vue
@@ -6,12 +6,22 @@
         <button @click="goToAddGroup" class="add-group top-header-btn" data-testId="add-group-btn" title="add group"><font-awesome-icon icon="fa-regular fa-plus" /></button>
       </div>
     </div>
+    <!-- NO GROUPS -->
     <div v-if="!groups || !groups.length" class="no-groups no-data">
       <p>You have no groups</p>
-      <p class="add-group-instruction">Click the <font-awesome-icon icon="fa-regular fa-plus" size="sm"/> to create your first group</p>
+      <p class="add-group-instruction">Click the <font-awesome-icon icon="fa-regular fa-plus" size="sm" /> to create your first group</p>
     </div>
+    <!-- GROUPS LIST -->
     <div v-else class="group-list">
-      <group-preview v-for="group in groups" :key="group.id" :group="group" @openMenu="toggleMenu" />
+      <!-- SEARCH GROUPS -->
+      <div class="search-group">
+        <input type="search" class="form-input search-input" v-model="searchTxt" />
+        <font-awesome-icon icon="fa-solid fa-magnifying-glass" class="magnifying-glass" />
+      </div>
+      <p v-if="!groupsToShow?.length" class="no-groups">No results</p>
+      <transition-group name="fade">
+        <group-preview v-for="group in groupsToShow" :key="group.id" :group="group" @openMenu="toggleMenu" />
+      </transition-group>
     </div>
     <transition name="menu-bottom" mode="out-in">
       <option-menu v-if="selectedGroup" :title="selectedGroup.name" @edit="editGroup" @remove="removeGroup" @close="toggleMenu" />
@@ -33,9 +43,18 @@ const router = useRouter();
 const groupStore = useGroupStore();
 
 const selectedGroup = ref(null);
+const searchTxt = ref(null);
 
 // COMPUTED
+
 const groups = computed(() => groupStore.groups);
+
+const groupsToShow = computed(() =>
+  groups.value.filter((g) => {
+    const rgx = new RegExp(searchTxt.value, 'i');
+    return searchTxt.value ? rgx.test(g.name) : g;
+  })
+);
 
 // FUNCTIONS
 function goToAddGroup() {
--- src/plugins/font-awesome.js
+++ src/plugins/font-awesome.js
@@ -1,9 +1,10 @@
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faCoins, faMessageLines, faUser, faAt, faLock, faUsers, faRightToBracket, faRightFromBracket, faGem, faCrown } from '@fortawesome/pro-thin-svg-icons'
 import { faPlus, faArrowLeft } from '@fortawesome/pro-regular-svg-icons'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faCoins, faMessageLines, faUser, faAt, faLock, faUsers, faRightToBracket, faRightFromBracket, faGem, faCrown, faPlus, faArrowLeft)
+library.add(faCoins, faMessageLines, faUser, faAt, faLock, faUsers, faRightToBracket, faRightFromBracket, faGem, faCrown, faPlus, faArrowLeft, faMagnifyingGlass)
 
 
 export {

3961f9a2dad6b9d8b7eb79ec7de6aff709c31ce0
RotemCarmon <tetch6@gmail.com>
2023-06-10 00:08:32 +0200

    Removed unused code

--- src/modules/expense/cmps/expense-list.vue
+++ src/modules/expense/cmps/expense-list.vue
@@ -5,43 +5,29 @@
         <p>You have no expenses yet</p>
       </div>
       <div v-else class="expense-list">
-        <expense-preview v-for="expense in expenses" :key="expense.id" :expense="expense" @openMenu="toggleMenu" />
+        <expense-preview v-for="expense in expenses" :key="expense.id" :expense="expense" @click="goToEditExpense(expense)" />
       </div>
     </main>
-    <!-- <div class="footer container">
-      <button @click="exportExpenses" class="btn dark bottom-btn">Export</button>
-    </div> -->
-    
-    <!-- OPTION MENU -->
-    <transition name="menu-bottom" mode="out-in">
-      <option-menu v-if="selectedExpense" :title="selectedExpense.description" @edit="goToEditExpense" @remove="removeExpense" @close="toggleMenu"> </option-menu>
-    </transition>
+    <div class="footer container">
+      <button @click="goToAddExpense" class="btn dark bottom-btn">Add Expense</button>
+    </div>
   </section>
 </template>
 
 <script setup>
 import expensePreview from './expense-preview.vue';
-import optionMenu from '@/modules/common/cmps/option-menu.vue';
-import { popupService } from '@/modules/common/services/popup.service.js';
-import getSymbolFromCurrency from 'currency-symbol-map';
 import { expenseService } from '../services/expense.service';
 import { computed, ref } from 'vue';
-import { useRoute, useRouter } from 'vue-router';
-import { useExpenseStore } from '../store';
-import { useGroupStore } from '../../group/store';
+import { useRouter } from 'vue-router';
 
+// SETUPS
 const props = defineProps({
   group: { type: Object },
 });
 
-// SETUPS
-const expenseStore = useExpenseStore();
-const groupStore = useGroupStore();
-const route = useRoute();
 const router = useRouter();
 
 // VARIABLES
-const group = ref(null);
 const selectedExpense = ref(null);
 
 // COMPUTED
@@ -64,25 +50,17 @@ function prepareExpenseReport(expense) {
   };
 }
 
-function toggleMenu(expense) {
-  selectedExpense.value = selectedExpense.value ? null : expense;
-}
-
-
 function findNameByEmailInGroup(email) {
   return Object.values(props.group.members).find((mem) => mem.email === email)?.name;
 }
-function goToEditExpense() {
-  router.push(`/expense/edit/${props.group.id}/${selectedExpense.value.id}?spender=${selectedExpense.value.spender}`);
+function goToEditExpense(expense) {
+  router.push(`/expense/edit/${props.group.id}/${expense.id}?spender=${expense.spender}`);
 }
-async function removeExpense() {
-  const expense = selectedExpense.value;
 
-  const isConfirm = await popupService.confirm({ title: 'Delete Expense', txt: `Are you sure you want to delete this expense?\n ${expense.description} ${getSymbolFromCurrency(expense.currency)}${expense.amount}`, approveTxt: 'Yes', cancelTxt: 'No' });
-  if (!isConfirm) return;
-
-  expenseStore.removeExpense({ expense, group: props.group });
+function goToAddExpense() {
+  router.push(`/expense/edit/${props.group.id}`);
 }
+
 function exportExpenses() {
   expenseService.exportExcel(props.group);
 }

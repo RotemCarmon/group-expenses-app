3eda2b117a4b85a6a4cfbc96f6b6c197cc22e8ff
RotemCarmon <tetch6@gmail.com>
2023-06-09 23:07:31 +0200

    Added member-balance-list cmp

--- src/modules/group/cmps/member-balance-list.vue
+++ src/modules/group/cmps/member-balance-list.vue
@@ -1,9 +1,39 @@
 <template>
   <section class="member-balance-list-container">
- <!-- <h1>Member balance list</h1> -->
+    <div class="member" v-for="(amount, member) in balances" :key="member">
+      <div class="name" data-testid="member-name">
+        {{ findNameByEmailInGroup(member, group) }}
+      </div>
+      <div class="break-down" :class="{ pos: amount > 0, neg: amount < 0 }" data-testid="member-amount">
+        {{ getSymbolFromCurrency(currency) }}
+        {{ parseFloat(amount.toFixed(2)) }}
+      </div>
+    </div>
   </section>
 </template>
 
 <script setup>
- 
+import { ref, watch } from 'vue';
+
+import { expenseService } from '@/modules/expense/services/expense.service';
+import { findNameByEmailInGroup } from '@/modules/common/services/util.service.js';
+import getSymbolFromCurrency from 'currency-symbol-map';
+
+const props = defineProps({
+  group: { type: Object },
+  currency: { type: String },
+});
+
+const balances = ref(null);
+
+// FUNCTIONS
+async function getBalances(userCurrency) {
+  balances.value = await expenseService.getBalances(props.group, userCurrency);
+}
+
+getBalances(props.currency);
+
+// watch(props.currency, (val) => {
+//   getBalances(val);
+// });
 </script>

68ff49bd09a2ec2b21bd30834b7666d3701b5cc9
RotemCarmon <tetch6@gmail.com>
2023-06-09 23:10:18 +0200

    Updated group-details cmp

--- src/modules/group/cmps/group-details.vue
+++ src/modules/group/cmps/group-details.vue
@@ -7,52 +7,34 @@
           <img src="@/assets/icons/ellipsis.svg" alt="ellipsis" />
         </div>
       </div>
-      <div class="member-list" v-if="balances">
+      <!-- <group-expense-summary :total="totalSpent" :currency="userCurrency" /> -->
+      <div class="group-expense-summary">
+        Total spent: <span>{{ getSymbolFromCurrency(userCurrency) }}{{ parseFloat(totalSpent.toFixed(2)) }}</span>
+      </div>
       <tab-nav />
-      <router-view />
-        <div class="member" v-for="(amount, member) in balances" :key="member">
-          <div class="name" data-testid="member-name">
-            {{ findNameByEmailInGroup(member, group) }}
-          </div>
-          <div class="break-down" :class="{ pos: amount > 0, neg: amount < 0 }" data-testid="member-amount">
-            {{ getSymbolFromCurrency(userCurrency) }}
-            {{ parseFloat(amount.toFixed(2)) }}
-          </div>
-        </div>
-      </div>
-      <div v-else class="loader-container">
-        <div class="app-loader"></div>
-      </div>
+      <router-view :group="group" :currency="userCurrency" :key="userCurrency" />
     </main>
-    <div v-if="balances" class="total-spent" data-testid="total-spent">
-      Total Spent
-      <span>{{ getSymbolFromCurrency(userCurrency) }} {{ parseFloat(totalSpent.toFixed(2)) }}</span>
-    </div>
 
     <!-- OPTION MENU -->
     <transition name="menu-bottom" mode="out-in">
-      <option-menu v-if="isMenuOpen" @edit="editGroup" @remove="removeGroup" @close="toggleMenu">
-        <template #content-top>
-          <div @click="goToAddExpense" class="line">Add Expense</div>
-          <div @click="goToExpenseList" class="line">Show All Expenses</div>
-        </template>
-      </option-menu>
+      <option-menu v-if="isMenuOpen" @edit="editGroup" @remove="removeGroup" @close="toggleMenu" />
     </transition>
   </section>
 </template>
 
 <script setup>
-import getSymbolFromCurrency from 'currency-symbol-map';
-import { expenseService } from '@/modules/expense/services/expense.service';
-import optionMenu from '@/modules/common/cmps/option-menu.vue';
-import tabNav from '@/modules/common/cmps/tab-nav.vue';
-import { popupService } from '@/modules/common/services/popup.service.js';
-import { findNameByEmailInGroup } from '@/modules/common/services/util.service.js';
-import { useRemoveGroup, goToEditGroup } from '@/composables/group.composable.js';
 import { ref, computed, watch } from 'vue';
+import { useRouter, useRoute } from 'vue-router';
+
 import { useAuthStore } from '@/modules/auth/store/auth.store';
 import { useGroupStore } from '../store/';
-import { useRouter, useRoute } from 'vue-router';
+
+import getSymbolFromCurrency from 'currency-symbol-map';
+import { expenseService } from '@/modules/expense/services/expense.service';
+import { useRemoveGroup, goToEditGroup } from '@/composables/group.composable.js';
+
+import optionMenu from '@/modules/common/cmps/option-menu.vue';
+import tabNav from '@/modules/common/cmps/tab-nav.vue';
 
 const authStore = useAuthStore();
 const groupStore = useGroupStore();
@@ -62,8 +44,6 @@ const route = useRoute();
 
 const group = ref(null);
 const totalSpent = ref(0);
-const summary = ref(null);
-const balances = ref(null);
 const isMenuOpen = ref(false);
 
 const loggedInUser = computed(() => authStore.loggedInUser);
@@ -71,18 +51,9 @@ const loggedInUser = computed(() => authStore.loggedInUser);
 const userCurrency = computed(() => loggedInUser.value?.prefs?.currency);
 
 // FUNCTIONS
-async function getTotalExpenses(userCurrency) {
+async function getTotalExpenses(userCurrency = authStore.loggedInUser.prefs.currency) {
   const { expenses } = group.value;
   totalSpent.value = await expenseService.getTotalExpenses(expenses, userCurrency);
-}
-
-async function getBalances(userCurrency) {
-  balances.value = await expenseService.getBalances(group.value, userCurrency);
-}
-
-function getSummeryData(userCurrency = authStore.loggedInUser.prefs.currency) {
-  getBalances(userCurrency);
-  getTotalExpenses(userCurrency);
 }
 
 function editGroup() {
@@ -91,18 +62,13 @@ function editGroup() {
 
 async function removeGroup() {
   const isRemoved = await useRemoveGroup(group);
-  if(isRemoved) router.push('/group/');
+  if (isRemoved) router.push('/group/');
 }
 
 function toggleMenu() {
   isMenuOpen.value = !isMenuOpen.value;
 }
-function goToExpenseList() {
-  router.push(`/expense/${group.value.id}`);
-}
-function goToAddExpense() {
-  router.push(`/expense/edit/${group.value.id}`);
-}
+
 async function getGroup() {
   const { groupId } = route.params;
   if (!groupId) return;
@@ -110,13 +76,9 @@ async function getGroup() {
   return group;
 }
 
-watch(userCurrency, (val) => {
-  getSummeryData(val);
-});
-
 async function created() {
   group.value = await getGroup();
-  getSummeryData();
+  getTotalExpenses();
 }
 
 created();

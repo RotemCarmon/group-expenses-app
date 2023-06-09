f0863a1ee3d878325856c38a5581655ade0cf9a0
RotemCarmon <tetch6@gmail.com>
2023-06-09 19:43:56 +0200

    Changed group preview style

--- src/modules/group/cmps/group-preview.vue
+++ src/modules/group/cmps/group-preview.vue
@@ -1,7 +1,7 @@
 <template>
-  <section class="group-preview-container preview-grid" @click.stop="goToGroupDetails" data-testid="group-preview">
+  <section class="group-preview-container" @click.stop="goToGroupDetails" data-testid="group-preview">
     <div class="group-name" data-testid="group-name">{{ group.name }}</div>
-    <div class="ellipsis-icon" @click.stop="$emit('openMenu', group)" data-testid="group-menu" >
+    <div class="ellipsis-icon" @click.stop="$emit('openMenu', group)" data-testid="group-menu">
       <img src="@/assets/icons/ellipsis.svg" alt="ellipsis" />
     </div>
   </section>

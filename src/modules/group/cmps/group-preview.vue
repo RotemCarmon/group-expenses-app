af756b4a36a1401bc4148f5176381032f2ec50bb
RotemCarmon <tetch6@gmail.com>
2023-06-10 15:13:11 +0200

    Changed group-preview style

--- src/modules/group/cmps/group-preview.vue
+++ src/modules/group/cmps/group-preview.vue
@@ -1,19 +1,25 @@
 <template>
   <section class="group-preview-container" @click.stop="goToGroupDetails" data-testid="group-preview">
     <div class="group-name" data-testid="group-name">{{ group.name }}</div>
-    <div class="ellipsis-icon" @click.stop="$emit('openMenu', group)" data-testid="group-menu">
-      <img src="@/assets/icons/ellipsis.svg" alt="ellipsis" />
-    </div>
+    <div class="group-created-at">{{ formatDate(group.createdAt, 'dd/MM/yy') }}</div>
+    <div class="group-member-count">Members: <span>{{ memberCount }}</span></div>
   </section>
 </template>
 
 <script setup>
+import { computed } from 'vue';
 import { useRouter } from 'vue-router';
+
+import { formatDate } from '@/modules/common/services/util.service';
 
 const router = useRouter();
 
 const props = defineProps({
   group: { type: Object },
+});
+
+const memberCount = computed(() => {
+  return Object.keys(props.group?.members ?? {}).length;
 });
 
 function goToGroupDetails() {

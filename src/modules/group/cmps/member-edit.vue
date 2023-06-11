725291a7acc44b6c7c70f4c8eb1488ea0277c329
RotemCarmon <tetch6@gmail.com>
2023-06-11 22:05:30 +0200

    Refactored member-edit cmp

--- src/modules/group/cmps/member-edit.vue
+++ src/modules/group/cmps/member-edit.vue
@@ -1,20 +1,18 @@
 <template>
-  <form @submit.prevent="save" class="member-edit-container main-app-container" aria-label="form" data-testId="edit-member-form" v-if="memberToEdit">
+  <form @submit.prevent="save" class="member-edit-container" aria-label="form" data-testId="edit-member-form" v-if="memberToEdit">
     <main>
-      <button @click="$emit('close')" class="close-btn">
-        <img src="@/assets/icons/close.svg" alt="close button" />
-      </button>
       <div class="page-header container">
         <div class="header-title">{{ isEdit ? 'Edit' : 'Add' }} Member</div>
+        <font-awesome-icon icon="fa-regular fa-xmark" @click="$emit('close')" />
       </div>
       <div class="form container">
         <div class="input-wrapper form-row">
           <font-awesome-icon icon="fa-thin fa-user" class="form-icon" />
-          <input aria-label="member-name" type="text" class="form-input" placeholder="Enter Name" v-model="memberToEdit.name" />
+          <input aria-label="member-name" type="text" class="form-input" placeholder="Enter Name" v-model="memberToEdit.name" ref="input" />
         </div>
         <div class="input-wrapper form-row">
           <font-awesome-icon icon="fa-thin fa-at" class="form-icon" />
-          <input aria-label="member-email" type="text" class="form-input" placeholder="Enter Email" v-model="memberToEdit.email" />
+          <input aria-label="member-email" type="text" class="form-input" placeholder="Enter Email" v-model="memberToEdit.email" :disabled="isEdit" />
         </div>
       </div>
     </main>
@@ -26,10 +24,12 @@
 
 <script setup>
 import { ref } from 'vue';
+
 const props = defineProps({
   member: { type: Object },
 });
-const emit = defineEmits(['save']);
+
+const emit = defineEmits(['save', 'close']);
 
 const memberToEdit = ref(props.member);
 const isEdit = ref(!!memberToEdit.value?.name);
